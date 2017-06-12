0cc403635536c7d2c7bc815742fc2020456ab493
alex <alex.o.keyes@gmail.com>
2017-06-12 17:00:37 -0400

    add optimal parity

--- primitive-types/cpp/parity.cpp
+++ primitive-types/cpp/parity.cpp
@@ -8,3 +8,16 @@ short Parity(unsigned long x) {
     }
     return result;
 }
+
+
+/* Here is the CPP for the best possible coding solution.  This is a fairly complicated method. Check page 30 for the
+ * solution explanation */
+short Parity(unsigned long x) {
+    x ^= x >> 32;
+    x ^= x >> 16;
+    x ^= x >> 8;
+    x ^= x >> 4;
+    x ^= x >> 2;
+    x ^= x >> 1;
+    return x & 0x1;
+}
